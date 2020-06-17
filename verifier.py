9297d083bdc5094395685b3ed136616a8babd164
Brandon Haynes <bhaynes@cs.washington.edu>
2020-06-17 14:00:33 -0700

    Update verified

--- verifier.py
+++ verifier.py
@@ -6,12 +6,12 @@ import tempfile
 import logging
 import argparse
 import numpy as np
+from sklearn.metrics import jaccard_similarity_score
 from itertools import zip_longest
 from common import *
 
 
 ALL_QUERIES = "1,2a,2b,2c,2d,3,4,5,6a,6b".split(',')
-
 
 def load_yaml(filename):
     with open(filename, 'r') as stream:
@@ -44,7 +44,6 @@ def get_writer(filename, reader, width=None, height=None):
 
 
 def assert_psnr(filename, reference_filename, threshold):
-    print(filename, reference_filename, threshold)
     subprocess.run(['./assert-psnr.sh', filename, reference_filename, str(threshold)])
 
 
@@ -114,7 +113,51 @@ def validate_q2b(dataset, query, result_filename):
 
 
 def validate_q2c(dataset, query, result_filename):
-    raise RuntimeError("Unimplemented")
+    segment_colors = {'pedestrian': (60, 20, 220), 'vehicle': (142, 0, 0)}  # BGR
+    objects = query['objects'] if 'objects' in query else ['pedestrian', 'vehicle']
+    threshold = 50
+
+    truth_reader = cv2.VideoCapture(os.path.join(dataset['path'], query['path']))
+    result_reader = cv2.VideoCapture(result_filename)
+    segmented_result = True
+    index = 0
+    iou_total = 0
+
+    while segmented_result:
+        segmented_result, segmented_frame = truth_reader.read()
+        result_result, result_frame = result_reader.read()
+        truth_frame= np.full_like(segmented_result, 0)
+        index += 1
+
+        if not result_result and segmented_result:
+            raise RuntimeError("Unexpected EOF in result video.")
+        elif not segmented_result and result_result:
+            raise RuntimeError("Too many frames in result video.")
+        elif segmented_result:
+            # Generate truth frame
+            for object in objects:
+                color = segment_colors[object]
+                thresholded = cv2.inRange(segmented_frame, tuple(t - threshold for t in color),
+                                                           tuple(t + threshold for t in color))
+                _, contours, hierarchy = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+                for contour in contours:
+                    x, y, w, h = cv2.boundingRect(contour)
+                    cv2.rectangle(truth_frame, (x, y), (x + w, y + h), color, cv2.FILLED)
+
+            # For each class, compare to result frame
+            for object in objects:
+                color = segment_colors[object]
+                thresholded_result = cv2.inRange(result_frame, color, color)
+                thresholded_truth = cv2.inRange(truth_frame, color, color)
+                iou = jaccard_similarity_score(thresholded_result, thresholded_truth)
+                iou_total += iou
+                if iou < JACCARD_THRESHOLD:
+                    print('FAIL: calculated Jaccard {} below limit of {} on frame {}'.format(
+                        iou, JACCARD_THRESHOLD, index))
+                    return
+
+    print('PASS: Mean Jaccard {}'.format(iou_total / index))
 
 
 def validate_q2d(dataset, query, result_filename):
